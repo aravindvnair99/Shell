f187184dd0bb054dd747f8ee09413585344e4bc6
Priya378 <59410993+Priya378@users.noreply.github.com>
2020-01-19 17:31:23 +0530

    Changed relational operator and typo
    corrected spelling mistake  and changed == operator to -eq
    
    Signed-off-by: Priya378 <bhuvanasree378@gmail.com>

--- Comparing two numbers.sh
+++ Comparing two numbers.sh
@@ -3,7 +3,7 @@
 echo "Enter any two numbers to compare"
 read a
 read b
-if [ $a == $b ]
+if [ $a -eq $b ]
 then
    echo "$a is equal to $b"
 elif [ $a -gt $b ]
@@ -13,5 +13,5 @@ elif [ $a -lt $b ]
 then
    echo "$a is less than $b"
 else
-   echo "Entered numbers do not satisy any conditions"
+   echo "Entered numbers do not satisfy any conditions"
 fi
