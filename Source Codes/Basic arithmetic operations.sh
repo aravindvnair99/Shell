0e7f3ade1c2596ba6aa1c81a892f99fa77ad33ab
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-01-24 00:09:34 +0530

    Style: Reformat
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- Source Codes/Basic arithmetic operations.sh
+++ Source Codes/Basic arithmetic operations.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 #A shell program to perform basic arithmetic operations
+
 echo "Enter any two positive numbers to perform basic arithmetic operations"
 read -r a
 read -r b
-c=$((a+b))
+c=$((a + b))
 echo "Sum is $c"
-if [ "$a" -gt "$b" ]
-then
-echo "Difference is $((a-b))"
+if [ "$a" -gt "$b" ]; then
+	echo "Difference is $((a - b))"
 else
-echo "Difference is $((b-a))"
+	echo "Difference is $((b - a))"
 fi
-echo "Product is $((b*a))"
-echo "Two possible quotients are $((a/b)) and $((b/a))"
+echo "Product is $((b * a))"
+echo "Two possible quotients are $((a / b)) and $((b / a))"
