b8456149d9038ff30ed1bb8a2d07a3e9b199a744
Priya378 <59410993+Priya378@users.noreply.github.com>
2020-01-26 16:06:56 +0530

    chore: Improvements
    * fix: restrict user input
    
    Signed-off-by: Priya378 <bhuvanasree378@gmail.com>
    
    * fix: modify user input
    
    Signed-off-by: Priya378 <bhuvanasree378@gmail.com>
    
    * style: correct spelling mistake
    
    Signed-off-by: Priya378 <bhuvanasree378@gmail.com>
    
    * style: modify output statements
    
    Signed-off-by: Priya378 <bhuvanasree378@gmail.com>
    
    * style: display message
    
    Signed-off-by: Priya378 <bhuvanasree378@gmail.com>

--- Source Codes/Basic arithmetic operations.sh
+++ Source Codes/Basic arithmetic operations.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
-
 #A shell program to perform basic arithmetic operations
-
 echo "Enter any two positive numbers to perform basic arithmetic operations"
+echo "Please enter 1st number"
 read -r a
+while [[ ! "$a" =~ ^[0-9]+$ ]];do
+        echo "Please enter only numbers (1st input):"
+        read -r a
+done
+echo "Please enter 2nd number"
 read -r b
-c=$((a + b))
-echo "Sum is $c"
-if [ "$a" -gt "$b" ]; then
-	echo "Difference is $((a - b))"
+while [[ ! "$b" =~ ^[0-9]+$ ]];do
+        echo "Please enter only numbers (2nd input):"
+        read -r b
+done
+echo "Sum is $((a+b))"
+if [ "$a" -gt "$b" ]
+then
+echo "Difference is $((a-b))"
 else
-	echo "Difference is $((b - a))"
+echo "Difference is $((b-a))"
 fi
-echo "Product is $((b * a))"
-echo "Two possible quotients are $((a / b)) and $((b / a))"
+echo "Product is $((b*a))"
+
+if [ "$b" -eq 0 ]
+then
+        echo "Division by 0 isn't defined, $a/$b is not possible"
+else
+        echo "Quotient when $a is divided by $b is $((a/b))"
+fi
+
+if [ "$a" -eq 0 ]
+then
+        echo "Division by 0 isn't defined, $b/$a is not possible"
+else
+        echo "Quotient when $b is divided by $a is $((b/a))"
+fi
--- Source Codes/Comparing two numbers.sh
+++ Source Codes/Comparing two numbers.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-
 #A shell program to compare two numbers
-
 echo "Enter any two numbers to compare"
+echo "Please enter 1st number"
 read -r a
+while  [[ ! "$a" =~ ^[-+]?[0-9]+$ ]]; do
+	echo "Please enter only numbers (1st input):"
+	read -r a
+done
+echo "Please enter 2nd number"
 read -r b
-if [ "$a" -eq "$b" ]; then
-	echo "$a is equal to $b"
-elif [ "$a" -gt "$b" ]; then
-	echo "$a is greater than $b"
-elif [ "$a" -lt "$b" ]; then
-	echo "$a is less than $b"
+while [[ ! "$b" =~ ^[-+]?[0-9]+$ ]]; do
+	echo "Please enter only numbers (2nd input):"
+	read -r b
+done
+if [ "$a" -eq "$b" ]
+then
+   echo "$a is equal to $b"
+elif [ "$a" -gt "$b" ]
+then
+   echo "$a is greater than $b"
+elif [ "$a" -lt "$b" ]
+then
+   echo "$a is less than $b"
 else
-	echo "Entered numbers do not satisfy any conditions"
+   echo "Entered numbers do not satisfy any conditions"
 fi
